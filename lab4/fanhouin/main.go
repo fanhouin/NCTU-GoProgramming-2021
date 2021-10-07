aacf767cdd7babcc41e0d6d9570746a5c2d73f1b
Fan <fanhouin@gamil.com>
2021-10-07 14:37:10 +0800

    done

--- lab4/fanhouin/main.go
+++ lab4/fanhouin/main.go
@@ -5,37 +5,49 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world!")
+	// fmt.Fprintf(w, "hello world!")
 
-	// 	pathParts := strings.SplitN(r.URL.Path, "/", -1)
+	pathParts := strings.SplitN(r.URL.Path, "/", -1)
+	// fmt.Println(pathParts)
 
-	// 	operation := pathParts[?]
-	// 	a, _ := strconv.Atoi(pathParts[?])
-	// 	b, _ := strconv.Atoi(pathParts[?])
+	operation := pathParts[1]
+	a, _ := strconv.Atoi(pathParts[2])
+	b, _ := strconv.Atoi(pathParts[3])
 
-	// 	if operation == "add" {
-
-	// 	} else if operation == "sub" {
-
-	// 	} else if operation == "mul" {
-
-	// 	} else if operation == "div" {
-
-	// 	} else {
-	// 		fmt.Fprintf(w, "hello world!")
-	// 	}
+	if operation == "add" {
+		fmt.Fprintf(w, "%d + %d = %d", a, b, add(a, b))
+	} else if operation == "sub" {
+		fmt.Fprintf(w, "%d - %d = %d", a, b, sub(a, b))
+	} else if operation == "mul" {
+		fmt.Fprintf(w, "%d * %d = %d", a, b, mul(a, b))
+	} else if operation == "div" && b != 0 {
+		di, re := div(a, b)
+		fmt.Fprintf(w, "%d / %d = %d, remainder = %d", a, b, di, re)
+	} else {
+		fmt.Fprintf(w, "hello world!")
+	}
 }
 
-// func add(a, b int) int {
+func add(a, b int) int {
+	return a + b
+}
 
-// }
+func sub(a, b int) int {
+	return a - b
+}
 
-// func div(a, b int) int int {
+func mul(a, b int) int {
+	return a * b
+}
 
-// }
+func div(a, b int) (int, int) {
+	return a / b, a % b
+}
 
 func main() {
 	port := "12345"
